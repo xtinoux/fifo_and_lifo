6675cc81ad988f6ce5be9481dfb7f97c74db5a93
eclave <etienne.clave@ac-mayotte.fr>
2019-06-16 17:12:22 +0300

    Modification de db_utils

--- db_utils.py
+++ db_utils.py
@@ -5,7 +5,7 @@ def recuperation_noms_etablissements():
     ''' pas de parametre
     retourne la liste des etablissements
     '''    
-    conn = sqlite3.connect('fifoandlifo.db')
+    conn = sqlite3.connect('static/db/fifoandlifo.db')
     cursor = conn.cursor() #  definie un curseur(pointeur) qui parcours la base
     cursor.execute("""SELECT etablissement FROM classes""")
     liste_etab = cursor.fetchall()
@@ -19,15 +19,16 @@ def niveau_evolution_classe(identifiant):
     ''' pas de parametre
     retourne la liste des etablissements
     '''
-    
-    
-    conn = sqlite3.connect('fifoandlifo.db')
+    conn = sqlite3.connect('static/db/fifoandlifo.db')
     cursor = conn.cursor() #  definie un curseur(pointeur) qui parcours la base
-    cursor.execute("""SELECT etablissement FROM classes""")
-    liste_etab = cursor.fetchall()
+
+
+    cursor.execute(f"""SELECT id_classe, niveau_enigme, echec_reussite_date FROM evolution WHERE id_classe={identifiant} """)
+    # formated string
+    evolution = cursor.fetchall()
     #print(liste_etab)
     conn.close()
-    return liste_etab   
+    return str(evolution)
 
 
 #print(recuperation_noms_etablissements())  
@@ -35,7 +36,11 @@ def niveau_evolution_classe(identifiant):
 if __name__ == '__main__':
     etabs = recuperation_noms_etablissements()
     print(etabs)
-    print(recuperation_noms_etablissements())
+    print(niveau_evolution_classe(2))
+    print(niveau_evolution_classe(5))
+    print(niveau_evolution_classe(1))
+    print(niveau_evolution_classe(4))
+    print(niveau_evolution_classe(3))
 
 
   
