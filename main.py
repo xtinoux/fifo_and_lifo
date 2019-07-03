641f5f15ee530664694418b7d4c23c1dd528e50a
eclave <etienne.clave@ac-mayotte.fr>
2019-07-03 15:38:27 +0300

    Modification de la fonction de test de l'enigme1

--- main.py
+++ main.py
@@ -36,21 +36,27 @@ def enigme1():
         """
         Effecute les tests de l'enigme
         """
-
-        return f"Résultat du test <br>  avec la fonction de tet qui ne sert à rien <br> et les parametres <br> {args}"
+        try:
+        	reulstat = 2 * args[0]
+        except:
+        	resultat = "La valeur passer en parametre est incorrect"
+        return resultat
 
     if request.method == 'POST':
     	reponse = request.form.get("reponse")
     	test_value = request.form.get("test")
+    	reponse_values = resquest.form.get("reponse")
     	
     	if reponse:
     		logging.info('Enregistrement de la réponse dans base de donnée')
     		test_result = "Reponse enregistée"
-    		return render_template("enigme1.html", test_result=test_result)
+    		update_db(id_classe, reponse_values)
+    		return render_template("enigme1.html", test_result="Résultat enregistée", final = True)
+    	
     	else:
     		logging.info('On effectue un test')
     		test_result = test(test_value)
-    		return render_template("enigme1.html", test_result=test_result)
+    		return render_template("enigme1.html", test_result=test_result, final=False)
 
     test_result = ""
     return render_template("enigme1.html", test_result=test_result)
