fcc25e4153c3854a25fb7aab464a4f8fa87d7760
eclave <etienne.clave@ac-mayotte.fr>
2019-07-05 05:55:50 +0300

    Externatilsation de la mozaique dans carousel.html

--- main.py
+++ main.py
@@ -113,4 +113,4 @@ def test():
 	return render_template('carousel.html', couleurs = VIOLETS, couleurs_d = VIOLETS_D)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8088, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
