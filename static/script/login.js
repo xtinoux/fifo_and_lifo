555f8314ed0f1e850d406b09bf90b519fac9b257
eric <e2cecco@gmail.com>
2019-07-08 14:21:58 +0300

    effacement des sections quand on modifie le choix du lycee

--- static/script/login.js
+++ static/script/login.js
@@ -18,7 +18,6 @@ function getSelectedLycee() {
 }
 
 function defRadioSection (ListeSection) {
-	// var form = document.getElementById("formsection");
 	var debut = ListeSection.length-1;//ListeSection.length-1;
 	// document.getElementById("demo").innerHTML = ListeSection.length;
 	var label = document.createElement("label");
