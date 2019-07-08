9b54590dbf29c292e7fb4fd6a7dc4f186069b8a5
Joris1968 <jorisdubruque@yahoo.fr>
2019-07-08 17:50:09 +0300

    a voir

--- app/db_utils.py
+++ app/db_utils.py
@@ -114,11 +114,7 @@ def idenfication(dico):
     ou alors suite aux tergiversation de l'autre la 
     donne uid si identification ok chaine vide si False (identification erronée)
     '''
-    
     uid = reccup_identifiant(dico['lycee'],dico['classe'])
-    # uid = reccup_identifiant('Lycée de Sada','NSI01')
-#
-    
     return identification2(uid,dico['password'])
     
 
@@ -198,7 +194,7 @@ if __name__ == '__main__':
     #dico2 = { 'lycee': 'Lycée de Sad' , 'classe': 'NSI01' , 'password': '1234'}
 
     print(recuperation_etab_et_classes())
-    #print(idenfication2(dico2))
+    print(idenfication(dico))
     # print(recuperation_etab_et_classes())
     # print(recuperation_noms_etablissements())
     # print(reccup_identifiant('Lycée de Sada','NSI02'))
@@ -206,6 +202,6 @@ if __name__ == '__main__':
     # print(reccup_identifiant('Lycée de Sada','NSI03'))
     # print(reccup_identifiant('Lycée de Kahani','NSI01'))
 
-    # print(enigmes_disponibles(4))
+    print(enigmes_disponibles(4))
     # raz_reponses_db()
     # print(enigmes_disponibles(4))
